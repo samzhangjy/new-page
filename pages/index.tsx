b84dc9be9fc1abf5826468676095ecd38f0dc80f
Sam Zhang <samzhangjy@outlook.com>
2022-10-22 10:40:04 +0800

    feat: offline notification

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,37 @@
 import { Container } from '@mantine/core';
+import { showNotification } from '@mantine/notifications';
+import { useEffect } from 'react';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle';
 import { Header } from '../components/Header';
 import { SearchBox } from '../components/SearchBox';
 
 export default function HomePage() {
+  const onOffline = () => {
+    showNotification({
+      title: "You're offline",
+      message:
+        'While you can use NTP just fine, you will not be able to search for anything until you connect to the Internet.',
+      color: 'red',
+    });
+  };
+
+  const onOnline = () => {
+    showNotification({
+      title: "You're back online!",
+      message: "Hooray! You're now back online.",
+      color: 'blue',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('online', onOnline);
+    window.addEventListener('offline', onOffline);
+
+    if (!window.navigator.onLine) {
+      onOffline();
+    }
+  });
+
   return (
     <>
       <Header />
--- public/fallback-GDGxsiH3pxJCa5HdQ9kLz.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";self.fallback=async e=>"document"===e.destination?caches.match("/",{ignoreSearch:!0}):Response.error()})();
